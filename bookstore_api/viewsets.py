9aa9506ca4803ed31e21be1e0d87e64824212bc2
afeez1131 <lawalafeez052@gmail.com>
2024-03-05 18:20:36 +0100

    pagination, filtering, searching

--- bookstore_api/viewsets.py
+++ bookstore_api/viewsets.py
@@ -6,4 +6,4 @@ from bookstore_api import pagination
 class CustomModelViewSets(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'delete']
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    pagination_class = pagination.StandardResultsSetPagination
+    pagination_class = pagination.CustomPagination
